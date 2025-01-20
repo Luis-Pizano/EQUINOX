f816b848f0ea6e2a1a0594e856220d420b56da03
Luis Pizano <luispizano883@gmail.com>
2025-01-20 15:18:12 -0300

    Se agrega barra bajo link de pagina donde usuario se encuentre

--- razas/urls.py
+++ razas/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('agregar_raza/', views.agregar_raza, name='agregar_razas'),
     path('listar_raza/', views.listar_razas, name='listar_razas'),
     path('eliminar_raza/<int:id>', views.eliminar_raza, name='eliminar_raza'),
-    path('presentacion_razas', views.presentacion_razas, name='presentacion_razas'),
+    path('presentacion_razas/', views.presentacion_razas, name='presentacion_razas'),
     path('editar_raza/<int:id_raza>', views.editar_raza, name='editar_raza'),
 
 
